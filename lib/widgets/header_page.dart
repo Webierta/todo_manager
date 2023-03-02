f651da5331b009343e7615252d3d18fa69969406
Webierta <webierta@gmail.com>
2023-03-02 18:33:40 +0100

    v2.0.4

--- lib/widgets/header_page.dart
+++ lib/widgets/header_page.dart
@@ -15,7 +15,7 @@ class HeaderPage extends StatelessWidget {
         ),
         Image.asset('assets/ic_launcher.png'),
         Text(
-          '${appLang.version} 2.0.3',
+          '${appLang.version} 2.0.4',
           style: Theme.of(context).textTheme.labelLarge,
         ),
         const SizedBox(height: 10),
